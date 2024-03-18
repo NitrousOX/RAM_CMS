bfa0a377c9d44ef3a8e7ea9ef52c0b4b2a1a8281
NitrousOX <poljvasdavid@gmail.com>
2024-03-18 21:31:33 +0100

    Login
    Login izgled

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -22,14 +22,26 @@ namespace RAM_CMS
         public Login()
         {
             InitializeComponent();
-            Label_Error.Visibility = Visibility.Hidden;
+            TextBlock_Error.Visibility = Visibility.Hidden;
+            TextBox_Username.Focus();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
-            this.Close();
+            if (String.IsNullOrEmpty(TextBox_Username.Text.ToString()) || String.IsNullOrEmpty(PasswordBox_Password.Password))
+            {
+                TextBlock_Error.Visibility = Visibility.Visible;
+                TextBlock_Error.Text = "Morate uneti username i password";
+
+            }
+            else
+            {
+                TextBlock_Error.Visibility = Visibility.Hidden;
+                TextBlock_Error.Text = "";
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -40,6 +52,23 @@ namespace RAM_CMS
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void TextBox_Username_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string username = TextBox_Username.Text.ToString();
+            if (!username.All(char.IsLetterOrDigit))
+            {
+                TextBox_Username.Text = username.Remove(username.Length - 1);
+                TextBox_Username.SelectionStart = username.Length - 1;
+                TextBlock_Error.Visibility = Visibility.Visible;
+                TextBlock_Error.Text = "Mozete uneti samo slova i brojeve za username";
+            }
+            else
+            {
+                TextBlock_Error.Visibility = Visibility.Hidden;
+                TextBlock_Error.Text = "";
+            }
         }
     }
 }
